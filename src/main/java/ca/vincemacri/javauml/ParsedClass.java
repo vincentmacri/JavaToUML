83eccac023d50fcfdad92fab9287a444d87da17d
quentincaffeino <sergey95zu@gmail.com>
2021-04-24 13:39:26 +0200

    Added pom; Updated to latest version of javaparser

--- src/main/java/ca/vincemacri/javauml/ParsedClass.java
+++ src/main/java/ca/vincemacri/javauml/ParsedClass.java
@@ -10,6 +10,7 @@ import java.util.stream.Stream;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Modifier;
 import com.github.javaparser.ast.NodeList;
+import com.github.javaparser.ast.Modifier.Keyword;
 import com.github.javaparser.ast.body.ConstructorDeclaration;
 import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
@@ -43,7 +44,8 @@ public class ParsedClass {
 	 * Get the parameters from the given {@link NodeList}.
 	 * 
 	 * @param nodeList The NodeList to extract parameters from.
-	 * @return A string formatted for UML output that contains all of the parameters.
+	 * @return A string formatted for UML output that contains all of the
+	 *         parameters.
 	 */
 	private String getParameterList(NodeList<Parameter> nodeList) {
 		StringBuilder parameters = new StringBuilder();
@@ -62,10 +64,11 @@ public class ParsedClass {
 	 * Check if any not wanted modifiers are in the modifiers list.
 	 * 
 	 * @param modifiers The modifiers present.
-	 * @param unwanted The modifiers to check against.
-	 * @return True if the present modifiers contain no unwanted modifiers, or false if they do.
+	 * @param unwanted  The modifiers to check against.
+	 * @return True if the present modifiers contain no unwanted modifiers, or false
+	 *         if they do.
 	 */
-	private boolean containsNone(EnumSet<Modifier> modifiers, Modifier[] unwanted) {
+	private boolean containsNone(NodeList<Modifier> modifiers, Modifier[] unwanted) {
 		for (Modifier avoid : unwanted) {
 			if (modifiers.contains(avoid)) {
 				return false;
@@ -75,14 +78,15 @@ public class ParsedClass {
 	}
 
 	/**
-	 * Convert a {@link Stream} of {@link FieldDeclaration} to an array of PlantUML formatted strings.
+	 * Convert a {@link Stream} of {@link FieldDeclaration} to an array of PlantUML
+	 * formatted strings.
 	 * 
 	 * @param validFields The fields to convert to PlantUML.
 	 * @return A string array of PlantUML fields.
 	 */
 	private String[] formatFields(Stream<FieldDeclaration> validFields) {
 		List<String> fields = new ArrayList<String>();
-		for (Iterator<FieldDeclaration> iterator = validFields.iterator(); iterator.hasNext(); ) {
+		for (Iterator<FieldDeclaration> iterator = validFields.iterator(); iterator.hasNext();) {
 			FieldDeclaration field = (FieldDeclaration) iterator.next();
 			for (VariableDeclarator variable : field.getVariables()) {
 				fields.add(variable.getNameAsString() + " : " + field.getCommonType());
@@ -90,61 +94,65 @@ public class ParsedClass {
 		}
 		return fields.toArray(new String[fields.size()]);
 	}
-	
+
 	/**
-	 * Convert a {@link Stream} of {@link MethodDeclaration} to an array of PlantUML formatted strings.
+	 * Convert a {@link Stream} of {@link MethodDeclaration} to an array of PlantUML
+	 * formatted strings.
 	 * 
 	 * @param validMethods The methods to convert to PlantUML.
 	 * @return A string array of PlantUML methods.
 	 */
 	private String[] formatMethods(Stream<MethodDeclaration> validMethods) {
 		List<String> methods = new ArrayList<String>();
-		for (Iterator<MethodDeclaration> iterator = validMethods.iterator(); iterator.hasNext(); ) {
+		for (Iterator<MethodDeclaration> iterator = validMethods.iterator(); iterator.hasNext();) {
 			MethodDeclaration method = (MethodDeclaration) iterator.next();
-			methods.add(method.getNameAsString() + "(" + getParameterList(method.getParameters()) + ") : " + method.getType());
+			methods
+					.add(method.getNameAsString() + "(" + getParameterList(method.getParameters()) + ") : " + method.getType());
 		}
 		return methods.toArray(new String[methods.size()]);
 	}
-	
+
 	/**
-	 * Convert a {@link Stream} of {@link ConstructorDeclaration} to an array of PlantUML formatted strings.
+	 * Convert a {@link Stream} of {@link ConstructorDeclaration} to an array of
+	 * PlantUML formatted strings.
 	 * 
 	 * @param validConstructors The constructors to convert to PlantUML.
 	 * @return A string array of PlantUML constructors.
 	 */
 	private String[] formatConstructors(Stream<ConstructorDeclaration> validConstructors) {
 		List<String> constructors = new ArrayList<String>();
-		for (Iterator<ConstructorDeclaration> iterator = validConstructors.iterator(); iterator.hasNext(); ) {
+		for (Iterator<ConstructorDeclaration> iterator = validConstructors.iterator(); iterator.hasNext();) {
 			ConstructorDeclaration method = (ConstructorDeclaration) iterator.next();
 			constructors.add(method.getNameAsString() + "(" + getParameterList(method.getParameters()) + ")");
 		}
 		return constructors.toArray(new String[constructors.size()]);
 	}
-	
+
 	/**
 	 * Get all of the fields based on the given arrays of {@link Modifier}.
 	 * 
-	 * @param wanted Array of wanted modifiers.
+	 * @param wanted   Array of wanted modifiers.
 	 * @param unwanted Array of all unwanted modifiers.
 	 * @return String array of all fields matching the specified conditions.
 	 */
 	public String[] getFields(Modifier[] wanted, Modifier[] unwanted) {
 		Stream<FieldDeclaration> fieldStream = unit.getNodesByType(FieldDeclaration.class).stream();
-		Stream<FieldDeclaration> validFields = fieldStream.filter(f -> f.getModifiers().containsAll(Arrays.asList(wanted)) && containsNone(f.getModifiers(), unwanted));
+		Stream<FieldDeclaration> validFields = fieldStream
+				.filter(f -> f.getModifiers().containsAll(Arrays.asList(wanted)) && containsNone(f.getModifiers(), unwanted));
 		return formatFields(validFields);
 	}
-
 
 	/**
 	 * Get all of the fields based on the given {@link Modifier}.
 	 * 
-	 * @param wanted The wanted modifier.
+	 * @param wanted   The wanted modifier.
 	 * @param unwanted The unwanted modifier.
 	 * @return String array of all fields matching the specified conditions.
 	 */
 	public String[] getFields(Modifier wanted, Modifier unwanted) {
 		Stream<FieldDeclaration> fieldStream = unit.getNodesByType(FieldDeclaration.class).stream();
-		Stream<FieldDeclaration> validFields = fieldStream.filter(f -> f.getModifiers().contains(wanted) && !f.getModifiers().contains(unwanted));
+		Stream<FieldDeclaration> validFields = fieldStream
+				.filter(f -> f.getModifiers().contains(wanted) && !f.getModifiers().contains(unwanted));
 		return formatFields(validFields);
 	}
 
@@ -163,26 +171,28 @@ public class ParsedClass {
 	/**
 	 * Get all of the methods based on the given {@link Modifier}.
 	 * 
-	 * @param wanted Array of wanted modifiers.
+	 * @param wanted   Array of wanted modifiers.
 	 * @param unwanted Array of all unwanted modifiers.
 	 * @return String array of all methods matching the specified conditions.
 	 */
 	public String[] getMethods(Modifier[] wanted, Modifier[] unwanted) {
 		Stream<MethodDeclaration> methodStream = unit.getNodesByType(MethodDeclaration.class).stream();
-		Stream<MethodDeclaration> validMethods = methodStream.filter(m -> m.getModifiers().containsAll(Arrays.asList(wanted)) && containsNone(m.getModifiers(), unwanted));
+		Stream<MethodDeclaration> validMethods = methodStream
+				.filter(m -> m.getModifiers().containsAll(Arrays.asList(wanted)) && containsNone(m.getModifiers(), unwanted));
 		return formatMethods(validMethods);
 	}
 
 	/**
 	 * Get all of the methods based on the given {@link Modifier}.
 	 * 
-	 * @param wanted The wanted modifier.
+	 * @param wanted   The wanted modifier.
 	 * @param unwanted The unwanted modifier.
 	 * @return String array of all methods matching the specified conditions.
 	 */
 	public String[] getMethods(Modifier wanted, Modifier unwanted) {
 		Stream<MethodDeclaration> methodStream = unit.getNodesByType(MethodDeclaration.class).stream();
-		Stream<MethodDeclaration> validMethods = methodStream.filter(m -> m.getModifiers().contains(wanted) && !m.getModifiers().contains(unwanted));
+		Stream<MethodDeclaration> validMethods = methodStream
+				.filter(m -> m.getModifiers().contains(wanted) && !m.getModifiers().contains(unwanted));
 		return formatMethods(validMethods);
 	}
 
@@ -194,33 +204,36 @@ public class ParsedClass {
 	 */
 	public String[] getMethods(Modifier[] wanted) {
 		Stream<MethodDeclaration> methodStream = unit.getNodesByType(MethodDeclaration.class).stream();
-		Stream<MethodDeclaration> validMethods = methodStream.filter(m -> m.getModifiers().containsAll(Arrays.asList(wanted)));
+		Stream<MethodDeclaration> validMethods = methodStream
+				.filter(m -> m.getModifiers().containsAll(Arrays.asList(wanted)));
 		return formatMethods(validMethods);
 	}
 
 	/**
 	 * Get all of the constructors based on the given {@link Modifier}.
 	 * 
-	 * @param wanted Array of wanted modifiers.
+	 * @param wanted   Array of wanted modifiers.
 	 * @param unwanted Array of all unwanted modifiers.
 	 * @return String array of all methods matching the specified conditions.
 	 */
 	public String[] getConstructors(Modifier[] wanted, Modifier[] unwanted) {
 		Stream<ConstructorDeclaration> constructorStream = unit.getNodesByType(ConstructorDeclaration.class).stream();
-		Stream<ConstructorDeclaration> validConstructors = constructorStream.filter(c -> c.getModifiers().containsAll(Arrays.asList(wanted)) && containsNone(c.getModifiers(), unwanted));
+		Stream<ConstructorDeclaration> validConstructors = constructorStream
+				.filter(c -> c.getModifiers().containsAll(Arrays.asList(wanted)) && containsNone(c.getModifiers(), unwanted));
 		return formatConstructors(validConstructors);
 	}
 
 	/**
 	 * Get all of the methods based on the given {@link Modifier}.
 	 * 
-	 * @param wanted The wanted modifier.
+	 * @param wanted   The wanted modifier.
 	 * @param unwanted The unwanted modifier.
 	 * @return String array of all methods matching the specified conditions.
 	 */
 	public String[] getConstructors(Modifier wanted, Modifier unwanted) {
 		Stream<ConstructorDeclaration> constructorStream = unit.getNodesByType(ConstructorDeclaration.class).stream();
-		Stream<ConstructorDeclaration> validConstructors = constructorStream.filter(c -> c.getModifiers().contains(wanted) && !c.getModifiers().contains(unwanted));
+		Stream<ConstructorDeclaration> validConstructors = constructorStream
+				.filter(c -> c.getModifiers().contains(wanted) && !c.getModifiers().contains(unwanted));
 		return formatConstructors(validConstructors);
 	}
 
@@ -232,7 +245,8 @@ public class ParsedClass {
 	 */
 	public String[] getConstructors(Modifier[] wanted) {
 		Stream<ConstructorDeclaration> constructorStream = unit.getNodesByType(ConstructorDeclaration.class).stream();
-		Stream<ConstructorDeclaration> validConstructors = constructorStream.filter(m -> m.getModifiers().containsAll(Arrays.asList(wanted)));
+		Stream<ConstructorDeclaration> validConstructors = constructorStream
+				.filter(m -> m.getModifiers().containsAll(Arrays.asList(wanted)));
 		return formatConstructors(validConstructors);
 	}
 }
